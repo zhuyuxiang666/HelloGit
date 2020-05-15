be49fbb8fd9b74059348866b66c276bc3a2588b5
zhuyuxiang <zhuyuxiang721@sina.com>
2020-05-15 23:28:43 +0800

    创建文件

---
+++ BBGItDemo/src/main/java/com/husor/HelloGIt.java
@@ -0,0 +1,13 @@
+package com.husor;
+
+/**
+ * @author X了个Y
+ * @create 2020-05-15-22:13
+ */
+public class HelloGIt {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello git!");
+    }
+}
